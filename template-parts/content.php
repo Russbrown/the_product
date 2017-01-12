f8f5c57b2f2f43fd98bade0cf757b5d8b2357a42
Russell Brown <emailrussellbrown@gmail.com>
2017-01-12 21:09:37 +0000

    home page posts styled

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,34 +8,15 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+
+	<div class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</div><!-- .entry-header -->
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php browny_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+	<?php $image = get_field("title_image"); ?>
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'browny' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
+	<img src="<?= $image['url'] ?>" class="entry-image"/>
 
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'browny' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+	<div class="entry-company"> <?= the_field("company") ?> </div>
 
-	<footer class="entry-footer">
-		<?php browny_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
