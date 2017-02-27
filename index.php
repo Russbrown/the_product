bb3047f6b27a4443585043bad850e13820d276a3
Russell Brown <emailrussellbrown@gmail.com>
2017-02-27 20:45:00 +0000

    tidied up mailchimp form

--- index.php
+++ index.php
@@ -47,21 +47,13 @@ get_header(); ?>
 
 		<?php endif; ?>
 
-			<!-- Begin MailChimp Signup Form -->
-			<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-			<div id="mc_embed_signup">
-			<form action="//theprdct.us15.list-manage.com/subscribe/post?u=e7510dfc6336ee69f4cbdd4be&amp;id=106483b035" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-				<div id="mc_embed_signup_scroll">
-				<label for="mce-EMAIL">Like this? Be the first to know about new interviews</label>
-				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-				<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e7510dfc6336ee69f4cbdd4be_106483b035" tabindex="-1" value=""></div>
-				<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-				</div>
-			</form>
-			</div>
-
-			<!--End mc_embed_signup-->
+		<form action="//theprdct.us15.list-manage.com/subscribe/post?u=e7510dfc6336ee69f4cbdd4be&amp;id=106483b035" method="post" name="mc-embedded-subscribe-form" class="validate signup-form" target="_blank" novalidate="" _lpchecked="1">
+			<label class="signup-label" for="mce-EMAIL">Like this? Be the first to know about new interviews</label>
+			<input class="signup-input" type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="">
+			<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e7510dfc6336ee69f4cbdd4be_106483b035" tabindex="-1" value=""></div>
+			<input type="submit" value="Subscribe" name="subscribe" class="signup-button" id="signup-button"/>
+		</form>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
